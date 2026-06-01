197dc53dd933d027c979e10f8c78b3de43192c07
Harshit Kumar Srivastav <harshitsrivastav644@gmail.com>
2026-06-02 03:09:19 +0530

    Add comment functionality to posts

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -19,7 +19,7 @@ const createPost = async (req, res) => {
   }
 };
 
-// GET ALL POSTS
+// GET POSTS
 const getPosts = async (req, res) => {
   try {
     const posts = await Post.find()
@@ -34,7 +34,69 @@ const getPosts = async (req, res) => {
   }
 };
 
+// LIKE POST
+const likePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({
+        message: "Post not found",
+      });
+    }
+
+    const alreadyLiked = post.likes.includes(req.user.id);
+
+    if (alreadyLiked) {
+      post.likes = post.likes.filter(
+        (id) => id.toString() !== req.user.id
+      );
+    } else {
+      post.likes.push(req.user.id);
+    }
+
+    await post.save();
+
+    res.status(200).json(post);
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+// COMMENT POST
+const commentPost = async (req, res) => {
+  try {
+    const { text } = req.body;
+
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({
+        message: "Post not found",
+      });
+    }
+
+    post.comments.push({
+      userId: req.user.id,
+      text,
+    });
+
+    await post.save();
+
+    res.status(200).json(post);
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
   createPost,
   getPosts,
+  likePost,
+  commentPost,
 };
