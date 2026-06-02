0a3a4712db06ef15691c299bf4a98a9d37ace6f6
Harshit Kumar Srivastav <harshitsrivastav644@gmail.com>
2026-06-02 22:07:18 +0530

    Signup Page create

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,100 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import api from "../services/api";
+import "./Register.css";
+
 function Register() {
-  return <h2>Register Page</h2>;
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    try {
+      await api.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+
+      alert("Registration Successful");
+
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      alert("Registration Failed");
+    }
+  };
+
+  return (
+    <div className="register-container">
+      <div className="register-card">
+        <h1>SocialSphere 🚀</h1>
+
+        <h2>Create Account</h2>
+
+        <form onSubmit={handleRegister}>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) =>
+              setUsername(e.target.value)
+            }
+            required
+          />
+
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) =>
+              setEmail(e.target.value)
+            }
+            required
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) =>
+              setPassword(e.target.value)
+            }
+            required
+          />
+
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChange={(e) =>
+              setConfirmPassword(e.target.value)
+            }
+            required
+          />
+
+          <button type="submit">
+            Sign Up
+          </button>
+        </form>
+
+        <p>
+          Already have an account?
+          <Link to="/"> Login</Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
