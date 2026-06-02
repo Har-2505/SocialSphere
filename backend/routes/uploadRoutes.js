a4ddb129d657fff2130e35c9709448c18560c8e6
Harshit Kumar Srivastav <harshitsrivastav644@gmail.com>
2026-06-02 23:12:29 +0530

    updateroutes

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -4,6 +4,8 @@ const upload = require("../middleware/uploadMiddleware");
 const router = express.Router();
 
 router.post("/", upload.single("image"), (req, res) => {
+  console.log(req.file);
+
   res.status(200).json({
     imageUrl: req.file.path,
   });
