9c8279dc0e78a32806614071a10f688af015f352
Harshit Kumar Srivastav <harshitsrivastav644@gmail.com>
2026-06-03 09:29:48 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-
+        <Route path="/profile" element={<Profile />} />
         <Route
           path="/register"
           element={<Register />}
